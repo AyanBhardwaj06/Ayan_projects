c61c5290f9cd551204d372460968990f13905481
AyanBhardwaj06 <124912964+AyanBhardwaj06@users.noreply.github.com>
2025-03-23 11:37:22 +0530

    Update Periphery & Big Block color.py

--- Periphery & Big Block color.py
+++ Periphery & Big Block color.py
@@ -4,47 +4,51 @@ def generate_grid(row, col, red, green, blue, periphery_order, block_color, bloc
     if row * col != red + green + blue:
         print("Error: The total number of colored cells does not match the grid size!")
         return []
-    
+
     grid = [[' ' for _ in range(col)] for _ in range(row)]
     color_counts = {'R': red, 'G': green, 'B': blue}
     positions = [(0, i) for i in range(col)] + [(row - 1, i) for i in range(col)] + [(i, 0) for i in range(1, row - 1)] + [(i, col - 1) for i in range(1, row - 1)]
     random.shuffle(positions)
-    
+
     for color in periphery_order:
         for x, y in positions:
             if grid[x][y] == ' ' and color_counts.get(color, 0) > 0:
                 grid[x][y] = color
                 color_counts[color] -= 1
-    
+
     max_possible_blocks = min(block_count, color_counts.get(block_color, 0) // (block_size ** 2))
     placed_blocks = 0
-    row_index = 1
+    available_rows = list(range(1, row - block_size))
+    random.shuffle(available_rows)
     
-    while row_index + block_size - 1 < row - 1 and placed_blocks < max_possible_blocks:
-        col_index = 1
-        while col_index + block_size - 1 < col - 1 and placed_blocks < max_possible_blocks:
+    for row_index in available_rows:
+        if placed_blocks >= max_possible_blocks:
+            break
+        available_cols = list(range(1, col - block_size))
+        random.shuffle(available_cols)
+        for col_index in available_cols:
+            if placed_blocks >= max_possible_blocks:
+                break
             if all(grid[r][c] == ' ' for r in range(row_index, row_index + block_size) for c in range(col_index, col_index + block_size)):
                 for r in range(row_index, row_index + block_size):
                     for c in range(col_index, col_index + block_size):
                         grid[r][c] = block_color
                 placed_blocks += 1
                 color_counts[block_color] -= (block_size ** 2)
-            col_index += block_size
-        row_index += block_size
     
     if placed_blocks < block_count:
         print(f"Warning: Could only place {placed_blocks} out of {block_count} blocks.")
-    
+
     empty_positions = [(r, c) for r in range(row) for c in range(col) if grid[r][c] == ' ']
     random.shuffle(empty_positions)
-    
+
     for r, c in empty_positions:
         available_colors = [color for color in color_counts if color_counts[color] > 0]
         if available_colors:
             chosen_color = random.choice(available_colors)
             grid[r][c] = chosen_color
             color_counts[chosen_color] -= 1
-    
+
     return grid
 
 row = int(input("Enter number of rows: "))
